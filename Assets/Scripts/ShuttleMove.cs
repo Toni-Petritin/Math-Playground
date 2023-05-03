e1a3704e73622bd4bfce140cc56f4e2f7249fe19
Toni-Petritin <ototi009@edu.xamk.fi>
2023-05-03 23:30:11 +0300

    Shuttle Controls

---
+++ Assets/Scripts/ShuttleMove.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ShuttleMove : MonoBehaviour
+{
+
+    public BezierPoint[] points; // For storing all bezier points
+    [Range(0f,1f)]
+    public float LapPos = 0.0f;
+
+    [SerializeField]
+    private GameObject prefab;
+
+    private void Awake()
+    {
+        Instantiate(prefab, this.transform);
+    }
+
+    void Update()
+    {
+
+        LapPos += Time.deltaTime * .1f;
+        if (LapPos >= 1)
+        {
+            LapPos = 0;
+        }
+
+        float roadSpot = LapPos * points.Length;
+        int roadSpotFloor = Mathf.FloorToInt(roadSpot);
+        int nextPoint = roadSpotFloor + 1;
+
+        if (roadSpotFloor >= points.Length - 1)
+        {
+            roadSpotFloor = points.Length - 1;
+            nextPoint = 0;
+        }
+
+
+        Vector3 tPos = GetBezierPosition(roadSpot - roadSpotFloor, points[roadSpotFloor], points[nextPoint]);
+        Vector3 tDir = GetBezierDirection(roadSpot - roadSpotFloor, points[roadSpotFloor], points[nextPoint]);
+
+        this.transform.position = tPos;
+        this.transform.rotation = Quaternion.LookRotation(tDir);
+
+    }
+
+
+    Vector3 GetBezierPosition(float t, BezierPoint bp1, BezierPoint bp2)
+    {
+
+        // 1st Lerp: 
+        Vector3 PtX = (1 - t) * bp1.Anchor.position + t * bp1.control1.position;
+        Vector3 PtY = (1 - t) * bp1.control1.position + t * bp2.control0.position;
+        Vector3 PtZ = (1 - t) * bp2.control0.position + t * bp2.Anchor.position;
+
+        // 2nd Lerp: 
+        Vector3 PtR = (1 - t) * PtX + t * PtY;
+        Vector3 PtS = (1 - t) * PtY + t * PtZ;
+
+        // 3rd Lerp:
+        return (1 - t) * PtR + t * PtS;
+    }
+
+    Vector3 GetBezierDirection(float t, BezierPoint bp1, BezierPoint bp2)
+    {
+
+        // 1st Lerp: 
+        Vector3 PtX = (1 - t) * bp1.Anchor.position + t * bp1.control1.position;
+        Vector3 PtY = (1 - t) * bp1.control1.position + t * bp2.control0.position;
+        Vector3 PtZ = (1 - t) * bp2.control0.position + t * bp2.Anchor.position;
+
+        // 2nd Lerp: 
+        Vector3 PtR = (1 - t) * PtX + t * PtY;
+        Vector3 PtS = (1 - t) * PtY + t * PtZ;
+
+        // Compute the direction vector
+        return (PtS - PtR).normalized;
+    }
+
+}
