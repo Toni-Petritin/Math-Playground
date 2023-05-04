b7b740091468d72a11a03d86b3d33eb921a98c81
Toni-Petritin <ototi009@edu.xamk.fi>
2023-05-04 18:01:37 +0300

    Shader Graph

--- Assets/Scripts/GeneratePlane.cs
+++ Assets/Scripts/GeneratePlane.cs
@@ -8,41 +8,37 @@ public class GeneratePlane : MonoBehaviour {
     [Range(1.0f, 1000.0f)]
     public float Size = 10.0f;
 
-    [Range(2, 500)]
+    [Range(2, 255)]
     public int Segments = 5;
 
+    // Generation variables
     [Range(0f,10000f)]
     public float offsetSeed = 0;
-
-    [Range(3,10)]
+    [Range(3, 10)]
     public float AmplitudeFirst = 5;
-
     [Range(3, 10)]
     public float DivFirst = 10;
-
     [Range(1, 3)]
     public float AmplitudeSecond = 2;
-
     [Range(1, 3)]
     public float DivSecond = 3;
-
     [Range(.1f, 1)]
     public float AmplitudeThird = 1;
-
     [Range(.1f, 1)]
     public float DivThird = 1;
-
     [Range(0f,5f)]
     public float platouLevel = 1;
-
+    [Range(0, 5)]
+    public float AmplitudeRock = 1;
     [Range(1f,10f)]
     public float DivRock1 = 4;
-
     [Range(1f, 4f)]
     public float DivRockCut1 = 2;
-    
-    [Range(0.01f, .5f)]
-    public float DivRockCut2 = .1f;
+    [Range(0.01f, 5f)]
+    public float DivRockBottom1 = .1f;
+
+    // For the shader
+    public Gradient terrainGradient;
 
     private Mesh mesh = null;
 
@@ -71,12 +67,7 @@ public class GeneratePlane : MonoBehaviour {
         float z = 0.0f;
         float y = 0.0f;
 
-        //float x_cent = ((seg_x * delta) - (Size * .5f)) * DoubleInverseSize;
-        //float z_cent = ((seg_z * delta) - (Size * .5f)) * DoubleInverseSize;
 
-        //crater = Mathf.Max(x_cent * x_cent, z_cent * z_cent);
-
-        //float noisevalue = Mathf.Clamp01(Mathf.PerlinNoise(x*.1f, z*.1f)) * NoiseFactor;
 
         for (int seg_x = 0; seg_x <= Segments; seg_x++) {
             x = (float)seg_x * delta + offsetSeed;
@@ -94,13 +85,22 @@ public class GeneratePlane : MonoBehaviour {
 
                 // Generate rocks
                 float y_rock1 = (Mathf.Max(Mathf.PerlinNoise(x / DivRock1, z / DivRock1), 0.7f) - 0.7f) * 10f;
-                float y_rock2 = (Mathf.Max(Mathf.PerlinNoise(x / DivRockCut1, z / DivRockCut1), 0.5f) - 0.5f) * 10f;
-                float y_rock3 = (Mathf.Max(Mathf.PerlinNoise(x / DivRockCut2, z / DivRockCut2), 0.3f) - 0.3f) * 2f;
-                float y_rock = Mathf.Max(0, y_rock1 - y_rock2 - y_rock3);
+                float y_rock2 = (Mathf.Max(Mathf.PerlinNoise(x / DivRockCut1, z / DivRockCut1), 0.5f) - 0.5f) * 8f;
+                float y_rock3 = Mathf.PerlinNoise(x / DivRockBottom1, z / DivRockBottom1) * 2f;
+                float y_rock = Mathf.Max(0, Mathf.Min( y_rock1 - y_rock2, y_rock3)) * AmplitudeRock;
 
                 y = Mathf.Max(platouLevel , y1 + y2 + y3) + y_rock;
 
                 verts.Add(new Vector3((float)seg_x * delta, y, (float)seg_z * delta));
+                if (y_rock > 0.01)
+                {
+                    colors.Add(terrainGradient.Evaluate(Mathf.Clamp01(y_rock)* .25f + .75f));
+                }
+                else
+                {
+                    float height = Mathf.InverseLerp(platouLevel, AmplitudeFirst + AmplitudeSecond + AmplitudeThird, y) * .7f;
+                    colors.Add(terrainGradient.Evaluate(height));
+                }
             }
         }
 
@@ -124,6 +124,7 @@ public class GeneratePlane : MonoBehaviour {
         }
         
         mesh.SetVertices(verts);
+        mesh.SetColors(colors);
         mesh.SetTriangles(tris, 0);
         mesh.RecalculateNormals();
     }
